7319837478e4a263172df1ac95db6338b88fce81
alexowusu <owusu3y@gmail.com>
2019-10-28 16:58:31 +0000

    lovely

--- src/io/turtabl/Main.java
+++ src/io/turtabl/Main.java
@@ -3,9 +3,28 @@ package io.turtabl;
 public class Main {
 
     //todo: work on the main method
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
          System.out.println("Hello, turntabl!");
+          if(args.length == 0){
+              System.out.print("please pass an argument");
+          }
+          else{
+              printArgs(args);
+          }
     }
+
+    public static void printArgs(String[] args){
+        for(String arg: args){
+            printArg(arg);
+        }
+    }
+    public static void printArg(String arg){
+        System.out.println(arg);
+    }
+
+
+
 }
 
 
